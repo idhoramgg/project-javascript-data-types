1a7c7451df285c4d1c2fe74b7cca8013fe010784
idhoramgg <ridhoabdulmajid@gmail.com>
2019-09-02 14:15:06 +0700

    clearing

--- index.js
+++ index.js
@@ -4,22 +4,25 @@
 // alert('Hello world')
 
 //string
-const name = 'Ridho Abdul Majid'
+// const name = 'Ridho Abdul Majid'
+
 //number
-const age = 25
+// const age = 25
+
 //object
-const bio = {
-  age = 25,
-  hobby: 'Reading',
-  isMarried: false,
-  activity : ['Read', 'Sleep', 'Code', 'Gaming']
-};
+// const bio = {
+//   age = 25,
+//   hobby: 'Reading',
+//   isMarried: false,
+//   activity : ['Read', 'Sleep', 'Code', 'Gaming']
+// };
+
 //calling obj
-console.log(bio.hobby);
-bio["age"] += 1;
+// console.log(bio.hobby);
+// bio["age"] += 1;
 
 //array
-const myHobbies = ['sleep', 'sleep', 'sleep'];
+// const myHobbies = ['sleep', 'sleep', 'sleep'];
 
 //calling array
 // console.log(myHobbies[0-2])
