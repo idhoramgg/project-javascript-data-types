9bbd0d326483407426e7e4cd748c8d7ca9a8e6eb
idhoramgg <ridhoabdulmajid@gmail.com>
2019-09-02 12:56:51 +0700

    index.js added some array

---
+++ README.md
@@ -0,0 +1,4 @@
+# learing javascript
+
+1.  var let and const;
+2.  object and array;
--- index.js
+++ index.js
@@ -3,15 +3,22 @@
 // console.log('Hello World')
 // alert('Hello world')
 
+//string
 const name = 'Ridho Abdul Majid'
+//number
 const age = 25
+//object
 const bio = {
   hobby: 'Reading',
   isMarried: false,
   activity : ['Read', 'Sleep', 'Code', 'Gaming']
 };
+//calling obj
+console.log(bio.hobby);
 
+//array
+const myHobbies = ['sleep', 'sleep', 'sleep'];
 
+//calling array
+// console.log(myHobbies[0-2])
 
-
-// console.log(ido);
