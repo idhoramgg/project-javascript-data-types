d34112601473dee73b188804b2ecedb9f9fb15c4
idhoramgg <ridhoabdulmajid@gmail.com>
2019-09-02 11:15:46 +0700

    add index.js

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 "use strict"
 
-console.log('Hello World')
-alert('Hello world')
+// console.log('Hello World')
+// alert('Hello world')
+
+const name = 'Ridho Abdul Majid'
+const age = 25
+const bio = {
+  hobby: 'Reading',
+  isMarried: false,
+  activity : ['Read', 'Sleep', 'Code', 'Gaming']
+};
+
+
+
+
+// console.log(ido);
