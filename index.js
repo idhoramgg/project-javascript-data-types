f55cb53f3de1d871d2ce0542839afd4584894f0f
idhoramgg <ridhoabdulmajid@gmail.com>
2019-09-03 09:21:44 +0700

    re-write comments

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
 "use strict"
 
-// console.log('Hello World')
-// alert('Hello world')
+console.log('Hello World')
+alert('Hello world')
 
-//string
-// const name = 'Ridho Abdul Majid'
+// string
+const name = 'Ridho Abdul Majid'
 
-//number
-// const age = 25
+// number
+const age = 25
 
-//object
-// const bio = {
-//   age = 25,
-//   hobby: 'Reading',
-//   isMarried: false,
-//   activity : ['Read', 'Sleep', 'Code', 'Gaming']
-// };
+// object
+const bio = {
+  age = 25,
+  hobby: 'Reading',
+  isMarried: false,
+  activities : ['Read', 'Sleep', 'Code', 'Gaming']
+};
 
-//calling obj
-// console.log(bio.hobby);
-// bio["age"] += 1;
+// calling obj
+console.log(bio.hobby);
+bio["age"] += 1;
 
-//array
-// const myHobbies = ['sleep', 'sleep', 'sleep'];
+// array
+const myHobbies = ['sleep', 'sleep', 'sleep'];
 
-//calling array
-// console.log(myHobbies[0-2])
+// calling array
+console.log(myHobbies[0-2])
 
+// json object catatan
+// change from JSON to object
+console.log(JSON.parse(variable));
+
+// change from object to JSON
+console.log(JSON.stringify(variable))
